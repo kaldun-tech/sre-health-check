78046f5ff55019c993ba9bbf36d488b058ba6a85
Taras <tsmereka@protonmail.com>
2023-12-29 09:17:26 -0600

    Main exits normally

--- scripts/endpoint_reader.py
+++ scripts/endpoint_reader.py
@@ -5,7 +5,7 @@ class EndpointReader:
     '''Reads REST endpoints from YAML file'''
 
     CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-    DEFAULT_FILE_PATH = os.path.join(CURRENT_DIR, '..', 'data', 'data/fetch.yml')
+    DEFAULT_FILE_PATH = os.path.join(CURRENT_DIR, '..', 'data', 'fetch.yml')
 
     def read_endpoints(file_path : str = DEFAULT_FILE_PATH):
         with open(file_path, 'r') as file:
---
+++ scripts/main.py
@@ -0,0 +1,14 @@
+from endpoint_reader import EndpointReader
+import sys
+
+def main():
+    '''Main entry point'''
+    endpoints = EndpointReader.read_endpoints()
+    if endpoints is None:
+        print('ERROR: Failed to read endpoints')
+        sys.exit(1)
+
+    sys.exit(0)
+
+if __name__ == '__main__':
+    main()
