eeaa85881cbf7b4f00a4548d7db8fb4836020802
Taras <tsmereka@protonmail.com>
2023-12-29 09:11:50 -0600

    First shot at reading the YAML file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # fetch-sre-health-check
 SRE Health Check Exercise for Fetch
+
+#### Install dependencies
+pip install pyyaml
---
+++ scripts/endpoint_reader.py
@@ -0,0 +1,13 @@
+import yaml
+import os
+
+class EndpointReader:
+    '''Reads REST endpoints from YAML file'''
+
+    CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+    DEFAULT_FILE_PATH = os.path.join(CURRENT_DIR, '..', 'data', 'data/fetch.yml')
+
+    def read_endpoints(file_path : str = DEFAULT_FILE_PATH):
+        with open(file_path, 'r') as file:
+            endpoints = yaml.safe_load(file)
+        return endpoints
