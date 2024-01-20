d421e1f58279f5631ece3d6c879db37d41d982fe
Taras <tsmereka@protonmail.com>
2024-01-19 20:42:04 -0600

    Implemented pause/run/exit keyboard commands

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
 '''SRE health check main functionality'''
 from argparse import ArgumentParser
+from pynput import keyboard
 import sys
 import time
 from scripts.endpoint_reader import read_endpoints
 from scripts.http_requests import HTTPRequester
 from scripts.availability import AvailabilityMetrics
+
+class RunState:
+    '''Class to hold run state'''
+    def __init__(self):
+        self.is_running = True
+
+    def is_running(self):
+        '''Checks whether program should continue to run'''
+        return self.is_running
+
+    def pause(self):
+        '''Pauses program'''
+        self.is_running = False
+        print('Program paused, press space to resume or Esc to quit...')
+
+    def resume(self):
+        '''Resumes execution'''
+        self.is_running = True
+        print('Execution resumed, press p to pause or Esc to quit...')
+
+    def on_release(self, key):
+        '''Listens for key press'''
+        if key == keyboard.Key.esc or key.char == 'x':
+            # Exit
+            sys.exit(0)
+        elif key == keyboard.Key.pause or key.char == 'p':
+            self.pause()
+        elif key == keyboard.Key.space or key.char == ' ':
+            self.resume()
+
 
 def do_query_report(metrics : AvailabilityMetrics, endpoints : dict):
     '''Query and report on endpoints'''
@@ -23,12 +54,18 @@ def main():
         print('ERROR: Failed to read endpoints')
         sys.exit(1)
 
+    print(f'Found {len(endpoints)} endpoints')
+    state = RunState()
     sleeptime = 15
     metrics = AvailabilityMetrics()
-    print(f'Found {len(endpoints)} endpoints')
-    print(f'Program will query endpoints and report results every {sleeptime} seconds, press CTRL + C to quit...')
+
+    listener = keyboard.Listener(on_release=state.on_release)
+    listener.start()
+
+    print(f'Program will query endpoints and report results every {sleeptime} seconds, press p to pause or x to quit...')
     while True:
-        do_query_report(metrics, endpoints)
+        if state.is_running:
+            do_query_report(metrics, endpoints)
         time.sleep(sleeptime)
 
 if __name__ == '__main__':
